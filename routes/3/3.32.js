a2b617d3efa59c17b7cde2fa48c090c13415add1
wuxiaoyang <15290491277@163.com>
2017-11-08 12:16:08 +0800

    11.7 study

--- routes/3/3.32.js
+++ routes/3/3.32.js
@@ -22,4 +22,4 @@ server.on('request',function (req,res) {
 server.listen(3000,'127.0.0.1');
 /*listenerCount接收2个参数，一个是指定哪个对象，第二个是指定该对象的哪个事件的事件处理函数的数量*/
 /*一定不要启动多个js文件 会发生端口占用*/
-console.log(events.EventEmitter.listenerCount(server,'request'));
+console.log(events.EventEmitter.listenerCount(server,'request')); // => 3
